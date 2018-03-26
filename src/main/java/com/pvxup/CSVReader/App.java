093321a12d4d9ebfa40519fc1befe24cc4fe2c00
Dai <kudzia.dawid@gmail.com>
2018-03-26 16:38:14 +0200

    Fixed the bug when the title of the bok had comma and because of that the CSV reader was reading the wrong data

--- src/main/java/com/pvxup/CSVReader/App.java
+++ src/main/java/com/pvxup/CSVReader/App.java
@@ -1,15 +1,17 @@
 package com.pvxup.CSVReader;
 
+import java.io.FileNotFoundException;
+
 public class App {
 	
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws FileNotFoundException{
 		
 		Reader reader = new Reader();
 		reader.addAuthorsAndTitles();
 		
 		CSV csv = new CSV("goodreads_library_export.csv");
-		csv.CSVReading(reader.authors);
+		csv.CSVAddAuthors(reader.authors);
 		
 		//System.out.println(reader.authors.size());
 		//System.out.println(reader.authors.values());
--- src/main/java/com/pvxup/CSVReader/CSV.java
+++ src/main/java/com/pvxup/CSVReader/CSV.java
@@ -1,45 +1,124 @@
 package com.pvxup.CSVReader;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 
 public class CSV {
 	
-	private static final String DEFAULT_SEPARATOR = ",";
+	private static final char DEFAULT_SEPARATOR = ',';
+	private static final char DEFAULT_QUOTE = '"';
 	
 	String csvFile = "";
+	Map<Integer, String> authors = new HashMap<Integer, String>();
 	
 	public CSV(String csvFile) {
 		this.csvFile = csvFile;
 	}
 	
-	public void CSVReading(Map<Integer, String> hm) {
-		 //String csvFile = "goodreads_library_export.csv";       
+	
+	 public static List<String> parseLine(String csvLine) {
 
-	        try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
-	        	
-	        	reader.readLine(); // This will read the first line
-	        	String line = null; 
-	        	
-	            while ((line = reader.readLine()) != null) { // loop starts from 2nd line
-	            
-	                String[] author = line.split(DEFAULT_SEPARATOR);
-	                author[3] = author[3].replace("\"", "");
-	                if (hm.containsValue(author[3])) {
-	                	System.out.println(author[3] + " is on the wishlist");
-	                }
-	                
-	               // System.out.println("Nazwisko: "+author[3]);
+	        List<String> result = new ArrayList<>();
 
-	     
-	            }
-	        } catch (IOException e) {
-	            e.printStackTrace();
+	        //if empty, return!
+	        if (csvLine == null && csvLine.isEmpty()) {
+	            return result;
 	        }
 
-	}
+	        StringBuffer curVal = new StringBuffer();
+	        boolean inQuotes = false;
+	        boolean doubleQuotesInColumn = false;
+
+	        char[] chars = csvLine.toCharArray();
+
+	        for (char ch : chars) {
+
+	            if (inQuotes) {
+	                if (ch == DEFAULT_SEPARATOR) {
+	                    inQuotes = false;
+	                    doubleQuotesInColumn = false;
+	                } else {
+
+	                    //Fixed : allow "" in custom quote enclosed
+	                    if (ch == DEFAULT_QUOTE) {
+	                        if (!doubleQuotesInColumn) {
+	                            curVal.append(ch);
+	                            doubleQuotesInColumn = true;
+	                        }
+	                    } else {
+	                        curVal.append(ch);
+	                    }
+
+	                }
+	            } else {
+	                if (ch == DEFAULT_SEPARATOR) {
+
+	                    inQuotes = true;
+
+	                } else if (ch == DEFAULT_SEPARATOR) {
+
+	                    result.add(curVal.toString());
+
+	                    curVal = new StringBuffer();
+
+	                } else if (ch == '\n') {
+	                    break;				//the end, break!
+	                } else {
+	                    curVal.append(ch);
+	                }
+	            }
+
+	        }        
+	        
+	        result.add(curVal.toString());
+	        
+	        return result;
+	    }
+	 
+	 private static String formatToLastName(String author) {
+			String auth = author;				// we don't want to change param author content
+			boolean moreThan1Word = false;		
+			int indexOfSpace = 0;				
+												   
+			for (int i = 0; i < auth.length(); i++) {
+				char letter = auth.charAt(i);
+				if (letter == ' ') {
+					moreThan1Word = true;
+					indexOfSpace = i;
+					break;
+				}
+			}
+			if (!moreThan1Word) 
+				return auth.substring(0, auth.length());	
+			else 
+				return auth.substring(indexOfSpace+1, auth.length());	
+		}
+	
+	public void CSVAddAuthors(Map<Integer, String> hm) throws FileNotFoundException {
+		
+		Scanner scanner = new Scanner(new File(csvFile));
+		int key = 0;
+		
+        while (scanner.hasNext()) {
+            List<String> line = parseLine(scanner.nextLine());
+            if (!(this.authors.containsValue(formatToLastName(line.get(2))))) {
+				this.authors.put(key, formatToLastName(line.get(2)));
+				key++;
+            }
+            //System.out.println("Last name: " + formatToLastName(line.get(2)));
+            if (hm.containsValue(formatToLastName(line.get(2))))
+            	System.out.println(line.get(2) + " is on the wishlist");
+            		
+        }
+        scanner.close();
+
+    }
 }
 
